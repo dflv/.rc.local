3e096587edd8c685179fe875563c2db598feda95
Dafu Lv <dlv@marvell.com>
2015-04-12 09:18:34 +0800

    Add "setup.sh"
    Signed-off-by: Dafu Lv <dlv@marvell.com>

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+BACKUP_SURFIX="-`date +"%Y%m%d%H%M%S"`"
+CONFIG_PREFIX="_"
+
+# replace configuration
+# $1 - local config folder
+# $2 - the core of the config file name
+_rep_conf()
+{
+    if [ -f ~/.$2 ];
+    then
+        mv ~/.$2 ~/.$2$BACKUP_SURFIX
+    fi
+    ln -s $PWD/$1/"$CONFIG_PREFIX"$2 ~/.$2
+}
+
+# ack
+_rep_conf ack ackrc
+# bash
+# git
+# openbox
+# ssh
+# tmux
+# vim
+_rep_conf vim vimrc.local
+_rep_conf vim vimrc.before.local
+# zsh
